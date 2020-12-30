35797fd14a44f71d648d2c475dcaa7cfc8327132
swjeong-912d <66402294+swjeong-912d@users.noreply.github.com>
2020-12-30 10:53:48 +0900

    P5 (L49) Group Anagrams 그룹별로 아나그램 묶기 문제. 책보고 수정한 버전 크게 차이나는건 defaultdict(list)와 ga 삭제, 예외처리 없애기와 value를 숫자대신 list로 할당한것. 확실히 이게 더 본질에 가까운 풀이법이고, 코드도 엄청 짧다.

--- L49.py
+++ L49.py
@@ -5,19 +5,9 @@ from typing import List
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ana_dict = defaultdict(list)
-        ga = [[]]
-        dict_no = 0
         for str in strs:
-            ss = "".join(sorted(str))
-            if not ss in ana_dict.keys():
-                ana_dict.update({ss:dict_no})
-                dict_no += 1
-            if len(ga) > ana_dict[ss]:
-                ga[ana_dict[ss]].append(str)
-            else:
-                ga.append([str])
-        return ga
-
+            ana_dict["".join(sorted(str))].append(str)
+        return ana_dict.values()
 def print_test():
     leet_sol = Solution()
     print(leet_sol.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
