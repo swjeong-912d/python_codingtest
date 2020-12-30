d15e60f6ddbffa6249d3a6cda096185d41b8ad87
swjeong-912d <66402294+swjeong-912d@users.noreply.github.com>
2020-12-30 10:55:14 +0900

    섹션 생성. 문제가 많아짐에 따라 디렉토리로 섹션의 문제를 구분하도록 변경.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from L49 import print_test
+from Section6_String.L49 import print_test
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
