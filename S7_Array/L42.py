71c6991ca2f51513aa64c94b5470c8a1d9df6e81
swjeong-912d <66402294+swjeong-912d@users.noreply.github.com>
2021-01-02 10:10:09 +0900

    P08 (L42) Trapping rain water
    스택은 생각 못함. 기본 룰은 같은데 스택으로 푸는게 있었네.
    재귀돌린거 좀만 다시보니 안돌려도 되는거였구만..

--- S7_Array/L42.py
+++ S7_Array/L42.py
@@ -9,15 +9,19 @@ class Solution:
                 sub_rain += max_h-height[j]
             return sub_rain
         def fill_left(b):
-            if b == 0:
-                return 0
-            c = max(range(b),key = lambda k:height[k])
-            return fill(c,b) + fill_left(c)
+            left_total = 0
+            while b > 0:
+                c = max(range(b),key = lambda k:height[k])
+                left_total += fill(c,b)
+                b = c
+            return left_total
         def fill_right(b):
-            if b == len(height)-1:
-                return 0
-            c = max(range(b+1,len(height)),key = lambda k:height[k])
-            return fill(b,c) + fill_right(c)
+            right_total = 0
+            while b < len(height)-1:
+                c = max(range(b+1,len(height)),key = lambda k:height[k])
+                right_total += fill(b,c)
+                b = c
+            return right_total
         if len(height) == 0:
             return 0
         max_h = max(range(len(height)),key = lambda k:height[k])
@@ -25,7 +29,7 @@ class Solution:
 
 def print_test():
     leet_sol = Solution()
-    print(leet_sol.trap([]))
+    print(leet_sol.trap([5, 1, 2, 3, 5]))
 
 
 
