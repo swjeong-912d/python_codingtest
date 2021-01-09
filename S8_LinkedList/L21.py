ee71087d77df73cd7f6a94dd6d75374230d57e7b
swjeong-912d <66402294+swjeong-912d@users.noreply.github.com>
2021-01-09 13:32:26 +0900

    P14 (L21) Merge Two Sorted Lists
    두 list를 합하는 문제.
    책이랑 같은 버전을 백트래킹 없이 시도해봄. 시행착오가 좀 많았는데, 결론적으로 가장 깔끔한 방법이라 생각함.

--- S8_LinkedList/L21.py
+++ S8_LinkedList/L21.py
@@ -7,46 +7,19 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        def merge(l_tail, l):
-            prev = l_tail
-            l_tail = ListNode()
-            l_tail.val = l.val
-            if prev != None:
-                prev.next = l_tail
-            return l_tail
-
-        if l1 is None and l2 is None:
-            return None
-        head = ListNode()
-        is_l1_head = True
-        if l1 is None or (l2 and l1 is not None and l1.val >= l2.val):
-            is_l1_head = False
-        if is_l1_head:
-            head.val = l1.val
-            l1 = l1.next
-        else:
-            head.val = l2.val
-            l2 = l2.next
-        tail = head
-        while l1 is not None and l2 is not None:
-            if l1.val <= l2.val:
-                tail = merge(tail, l1)
-                l1 = l1.next
-            else:
-                tail = merge(tail, l2)
-                l2 = l2.next
-
-        while l1 is not None:
-            tail = merge(tail, l1)
-            l1 = l1.next
-            if head == None:
-                head = tail
-        while l2 is not None:
-            tail = merge(tail, l2)
-            l2 = l2.next
-            if head == None:
-                head = tail
-        return head
+        if l1 is None:
+            l1, l2 = l2, l1
+        elif l2 is not None and l2.val < l1.val:
+            l1, l2 = l2, l1
+        lc = l1
+        while lc is not None and l2 is not None:
+            if lc.next is not None and lc.next.val > l2.val:
+                lc.next, l2 = l2, lc.next
+            elif lc.next is None and l2 is not None:
+                lc.next = l2
+                break
+            lc = lc.next
+        return l1
 
 
 def make_LL(array):
@@ -75,4 +48,4 @@ def print_LL(head):
 
 def print_test():
     leet_sol = Solution()
-    print_LL(leet_sol.mergeTwoLists(make_LL([1, 2, 4]), make_LL([1, 3, 4])))
+    print_LL(leet_sol.mergeTwoLists(make_LL([1]), make_LL([])))
