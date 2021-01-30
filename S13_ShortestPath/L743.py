93e4200b5bf845027cd683ddc5218b8d7e4de93e
SEWON <sw912d@gmail.com>
2021-01-30 09:46:59 +0900

    L743. Network Delay Time 다익스트라 구현. heapq도 쓰고, 만족함. 첨에 한칸씩 다 q에 넣었는데, 생각해보니 source 하나만 넣고 시작하는게 맞아서 변경.

---
+++ S13_ShortestPath/L743.py
@@ -0,0 +1,31 @@
+import heapq
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        path = [float('inf')] * (n + 1)
+        path[k] = 0
+        edges = defaultdict(list)
+        for e in times:
+            edges[e[0]].append(e[1:])
+        pque = []
+        heapq.heappush(pque, (path[k], k))
+        while len(pque) > 0:
+            u = heapq.heappop(pque)[1]
+            for vw in edges[u]:
+                v = vw[0]
+                w = vw[1]
+                if path[u] + w < path[v]:
+                    path[v] = path[u] + w
+                    heapq.heappush(pque, (path[vw[0]], vw[0]))
+        if max(path[1:]) == float('inf'):
+            return -1
+        else:
+            return max(path[1:])
+
+
+def print_test():
+    leet_sol = Solution()
+    print(leet_sol.networkDelayTime([[1, 2, 1], [2, 1, 3]], 2, 2))
