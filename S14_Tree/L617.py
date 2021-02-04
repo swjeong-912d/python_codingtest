567c4f8da0914d3f6f279425a086283e3e807ffd
SEWON <sw912d@gmail.com>
2021-02-04 10:46:46 +0900

    L617. Merge Two Binary Trees 어려운건 없는데, left right 바꿔서 왜 안되나 한참 고민함. 이상한데서 말리지 맙시다.

---
+++ S14_Tree/L617.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if t1 and t2:
+            t1.val += t2.val
+            t1.right = self.mergeTrees(t1.right, t2.right)
+            t1.left = self.mergeTrees(t1.left, t2.left)
+        elif not t1 and t2:
+            t1 = t2
+        return t1
