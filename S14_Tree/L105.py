598c7a7f355a2996939a64d77190756c73ddc912
swjeong-912d <66402294+swjeong-912d@users.noreply.github.com>
2021-02-15 18:56:14 +0900

    105. Construct Binary Tree from Preorder and Inorder Traversal preorder에 대한 개념만 잘 알고 있고, 이놈이 해도 되는지 하면 안되는지 여부만 잘 파악해서 재귀로 실행하면 됨. 속도도 느리고, 메모리도 많이 잡아먹지만 어쨌든 깨긴 깼음. 고민끝에 정석대로 푼거라 나름 만족.

---
+++ S14_Tree/L105.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+from typing import List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        def buildTree_pre(node,start,end):
+            if start < end:
+                node = TreeNode(preorder[self.ind])
+                self.ind += 1
+                mid = inorder.index(node.val,start,end)
+                node.left = buildTree_pre(node.left,start,mid)
+                node.right = buildTree_pre(node.right,mid+1,end)
+                return node
+            else:
+                return None
+        if not preorder:
+            return None
+        root = None
+        self.ind = 0
+        return buildTree_pre(root,0,len(preorder))
+
+def print_test():
+    leet_sol = Solution()
+    leet_sol.buildTree([3,9,20,15,7], [9,3,15,20,7])
